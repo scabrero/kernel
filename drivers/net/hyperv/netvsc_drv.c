af779b7579d611b414cc73485d63c07fc8b9864f
Olaf Hering <ohering@suse.de>
2018-11-06 10:54:57 +0100

    use the new async probing feature for the hyperv drivers (bsc#1109772).
    suse-commit: ba6543f8922ab4ece22c167807a45a8841473997

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -2170,6 +2170,9 @@ static struct  hv_driver netvsc_drv = {
 	.id_table = id_table,
 	.probe = netvsc_probe,
 	.remove = netvsc_remove,
+	.driver = {
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
+	},
 };
 
 /*
